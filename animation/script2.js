90942fc8799e4e51a650b9860dfa6569a923c5f0
silso <chroelo@gmail.com>
2017-11-20 23:59:26 -0600

    made animation work!
    still needs comments and cleanup though

--- animation/script2.js
+++ animation/script2.js
@@ -1,82 +1,60 @@
 var HEIGHT = window.innerHeight;
 var WIDTH = window.innerWidth;
-var SCALE = 2;
+var SCALE = 4;
 
-var G = -980;
+var G = -1/2000;
 var FPS = 72;
-var K = .1;
-G /= Math.pow(FPS, 2);
+var K = 1;
 
 var now = Date.now();
 var then = Date.now();
 var interval = 1000 / FPS;
 var delta = now - then;
 
-//create a global clock to solve the desync issue. At the each setStart, have the objects take note of their start time. Then just find the current time by subtracting global clock from that start time. should implement easily. i doubt you'll be able to solve the current desync issue.
+var printCount = 0;
+var print = function(string) {
+	if (printCount++ < 300) {
+		console.log(string);
+	}
+}
 
 var Hand = function(hM) {
 	
 	this.setStart = function() {
-		this.ti = now/1000;
-		this.t = this.ti;
-		
 		this.j = (this.i + 1) % hM.length;
 		
 		ih = hM[this.i];
 		jh = hM[this.j];
-		
-		this.p = {
-			x:
-				ih.p.x,
-			y:
-				ih.p.y
-		}
-		/*this.v = {
-			x:
-				ih.v.x,
-			y:
-				ih.v.y
-		}
-		this.a = {
-			x:
-				2 * (-3*ih.p.x + 3*jh.p.x - 2*ih.v.x*ih.t - jh.v.x*ih.t) / Math.pow(ih.t, 2),
-			y:
-				2 * (-3*ih.p.y + 3*jh.p.y - 2*ih.v.y*ih.t - jh.v.y*ih.t) / Math.pow(ih.t, 2)
-		}
-		this.j = {
-			x:
-				6 * (2*ih.p.x - 2*jh.p.x + ih.v.x*ih.t + jh.v.x*ih.t) / Math.pow(ih.t, 3),
-			y:
-				6 * (2*ih.p.y - 2*jh.p.y + ih.v.y*ih.t + jh.v.y*ih.t) / Math.pow(ih.t, 3)
-		}*/
 	}	
 	
 	this.move = function() {
-		this.t = (now/1000 - this.ti);
-		if (this.t < ih.t) {
-			this.p.x = (2*ih.p.x - 2*jh.p.x + ih.v.x*ih.t + jh.v.x*ih.t) / Math.pow(ih.t, 3) * 
-				Math.pow(this.t, 3) + 
-				(-3*ih.p.x + 3*jh.p.x - 2*ih.v.x*ih.t - jh.v.x*ih.t) / Math.pow(ih.t, 2) * 
-				Math.pow(this.t, 2) + 
-				ih.v.x*this.t + 
-				ih.p.x;
-			this.p.y = (2*ih.p.y - 2*jh.p.y + ih.v.y*ih.t + jh.v.y*ih.t) / Math.pow(ih.t, 3) * 
-				Math.pow(this.t, 3) + 
-				(-3*ih.p.y + 3*jh.p.y - 2*ih.v.y*ih.t - jh.v.y*ih.t) / Math.pow(ih.t, 2) * 
-				Math.pow(this.t, 2) + 
-				ih.v.y*this.t + 
-				ih.p.y;
-		}
-		else {
+		this.t = (now/1000 - this.ti) % hM[hM.length - 1].t;		//same thing as Ball.t
+		var tt = this.t - ih.ti;
+		var ihtt = K*(ih.t - ih.ti);
+		if (this.t < ih.ti || this.t > ih.t) {
 			this.i = (this.i + 1) % hM.length;
 			this.setStart();
-			//this.move();
+		} else {
+			this.p.x = 
+				(2*ih.p.x - 2*jh.p.x + ih.v.x*ihtt + jh.v.x*ihtt) / Math.pow(ihtt, 3) * 
+				tt*tt*tt + 
+				(-3*ih.p.x + 3*jh.p.x - 2*ih.v.x*ihtt - jh.v.x*ihtt) / Math.pow(ihtt, 2) * 
+				tt*tt + 
+				ih.v.x*tt + 
+				ih.p.x;
+			this.p.y = 
+				(2*ih.p.y - 2*jh.p.y + ih.v.y*ihtt + jh.v.y*ihtt) / Math.pow(ihtt, 3) * 
+				tt*tt*tt + 
+				(-3*ih.p.y + 3*jh.p.y - 2*ih.v.y*ihtt - jh.v.y*ihtt) / Math.pow(ihtt, 2) * 
+				tt*tt + 
+				ih.v.y*tt + 
+				ih.p.y;
 		}
 	}
 	
 	this.draw = function(ctx) {
 		ctx.save();
-		ctx.fillStyle = "#005500";
+		ctx.fillStyle = "#000000";
 		ctx.beginPath();
 		ctx.arc(WIDTH/2+SCALE*this.p.x,
 			HEIGHT/2-SCALE*this.p.y, 
@@ -89,75 +67,74 @@ var Hand = function(hM) {
 		ball.setPos(this.p);
 	}
 	
+	this.init = function(time) {
+		this.ti = time/1000;
+		
+		this.p = {};
+		
+		this.i = 0;
+		this.setStart();
+	}
+	
 	var ih;
 	var jh;
-	
-	this.i = 0;
-	this.setStart();
 }
 
 var Ball = function(color, radius, bM) {
 	
 	this.setStart = function() {
-		var i = this.i;
-		
-		this.ti = now/1000;
-		this.t = 0;
-		this.tc = bM[i].throw.t;
-		this.tf = this.tc + bM[i].catch.t;
-		
-		this.x = bM[i].throw.p.x;
-		this.y = bM[i].throw.p.y;
-		this.xf = bM[i].catch.p.x;
-		this.yf = bM[i].catch.p.y;
-		this.vx = (bM[i].catch.p.x - bM[i].throw.p.x) / (this.tc*FPS);
-		this.vy = (-.5*G*Math.pow(this.tc*FPS, 2) + bM[i].catch.p.y - bM[i].throw.p.y) / (this.tc*FPS);
+		this.tci = bM[this.i].throw.ti;
+		this.tc = bM[this.i].throw.t;
+		this.tf = bM[this.i].catch.t;
 	}
 	
 	this.move = function() {
-		this.t = (now/1000 - this.ti);
-		if (this.t < this.tc) {
-			this.x += this.vx;
-			this.y += this.vy;
-			this.vy += G;
-		} else if(this.t < this.tf) {
-			bM[this.i].catch.h.grab(this);
-		} else {
+		this.t = (now - this.ti + this.offset) % bM[bM.length - 1].catch.t;		//setting t to now relative to the length of time of the loop
+		if (this.t > this.tf || this.t < this.tci) {				//if this.t is outside of range of ballMovement
 			this.i = (this.i + 1) % bM.length;
 			this.setStart();
-			//this.move();
-			/*this.x = this.xf;
-			this.y = this.yf;
-			this.t = this.tf;*/
+		} else if (this.t < this.tc) {
+			this.p.x = 
+				((bM[this.i].catch.p.x - bM[this.i].throw.p.x) / (this.tc - this.tci))*(this.t - this.tci) + 
+				bM[this.i].throw.p.x;
+			this.p.y = 
+				.5*G*(this.t - this.tci)*(this.t - this.tci) + 
+				((-.5*G*(this.tc - this.tci)*(this.tc - this.tci) + bM[this.i].catch.p.y - bM[this.i].throw.p.y) / (this.tc - this.tci))*(this.t - this.tci) + 
+				bM[this.i].throw.p.y;
+		} else {
+			bM[this.i].catch.h.grab(this);
 		}
 	}
 	
 	this.setPos = function(p) {
-		this.x = p.x;
-		this.y = p.y;
+		this.p.x = p.x;
+		this.p.y = p.y;
 	}
 	
 	this.draw = function(ctx) {
 		ctx.save();
 		ctx.fillStyle = this.c;
 		ctx.beginPath();
-		ctx.arc(WIDTH / 2 + SCALE * this.x, 
-				HEIGHT / 2 - SCALE * this.y, 
+		ctx.arc(WIDTH / 2 + SCALE * this.p.x, 
+				HEIGHT / 2 - SCALE * this.p.y, 
 				SCALE * this.r / 2, 0, 2 * Math.PI);
 		ctx.fill()
 		ctx.restore();
 	}
 	
-	
-	//
-	
-	
-	this.c = color;
-	this.r = radius;
-	
-	this.i = 0;
-	
-	this.setStart();
+	this.init = function(time, offset) {		
+		this.ti = time;
+		this.offset = offset;
+		
+		this.p = {};
+		
+		this.c = color;
+		this.r = radius;
+		
+		this.i = 0;
+		
+		this.setStart();
+	}
 }
 
 
@@ -173,10 +150,11 @@ function init() {
 				y:0
 			},
 			v: {
-				x:0,
+				x:50,
 				y:200
 			},
-			t:1
+			ti:0,
+			t:.5
 		},
 		{
 			p: {
@@ -184,14 +162,45 @@ function init() {
 				y:0
 			},
 			v: {
-				x:-10,
-				y:-50
+				x:-50,
+				y:-180
 			},
+			ti:.5,
+			t:1
+		}
+	];
+	var handMovements2 = [
+		{
+			p: {
+				x:35,
+				y:0
+			},
+			v: {
+				x:50,
+				y:-180
+			},
+			ti:0,
 			t:.5
+		},
+		{
+			p: {
+				x:5,
+				y:0
+			},
+			v: {
+				x:-50,
+				y:180
+			},
+			ti:.5,
+			t:1
 		}
 	];
 	
-	leftHand = new Hand(handMovements);
+	var leftHand = new Hand(handMovements);
+	var rightHand = new Hand(handMovements2);
+	
+	leftHand.init(now);
+	rightHand.init(now);
 	
 	var ballMovements = [
 		{
@@ -200,20 +209,51 @@ function init() {
 					x:-5,
 					y:0
 				},
-				t:1
+				ti:0,
+				t:1000
+			},
+			catch: {
+				p: {
+					x:35,
+					y:0
+				},
+				t:1500,
+				h:rightHand
+			}
+		},
+		{
+			throw: {
+				p: {
+					x:5,
+					y:0
+				},
+				ti:1500,
+				t:2500
 			},
 			catch: {
 				p: {
 					x:-35,
 					y:0
 				},
-				t:.5,
+				t:3000,
 				h:leftHand
 			}
 		}
-	];	
+	];
 	
-	var ball = new Ball("#FF0000", 7, ballMovements);
+	var balls = [
+		new Ball("#FF0000", 7, ballMovements),
+		new Ball("#00FF00", 7, ballMovements),
+		new Ball("#0000FF", 7, ballMovements)
+	];
+	
+	balls[0].init(now, 0);
+	balls[1].init(now, 2000);
+	balls[2].init(now, 4000);
+	
+	
+	//
+	
 	
 	function draw() {
 		requestAnimationFrame(draw);
@@ -231,9 +271,11 @@ function init() {
 			ctx.clearRect(0, 0, WIDTH, HEIGHT);
 			
 			leftHand.draw(ctx);
-			ball.draw(ctx);
+			rightHand.draw(ctx);
+			for (var i = 0; i < balls.length; i++) balls[i].draw(ctx);
 			leftHand.move();
-			ball.move();
+			rightHand.move();
+			for (var i = 0; i < balls.length; i++) balls[i].move();
 		}
 	}
 	
